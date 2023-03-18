a81b5c00dcc0eb3cea1a9406c83f9bf5ea69b792
Leafenzo <leafenzo@gmail.com>
2023-03-17 22:45:45 -0600

    DataGen working

--- src/main/java/net/leafenzo/squashed/block/GunpowderWireBlock.java
+++ src/main/java/net/leafenzo/squashed/block/GunpowderWireBlock.java
@@ -1,20 +1,23 @@
 package net.leafenzo.squashed.block;
 
 import net.minecraft.block.RedstoneWireBlock;
+
 public class GunpowderWireBlock extends RedstoneWireBlock {
     public GunpowderWireBlock(Settings settings) {
         super(settings);
     }
 
-
 //    Placed Gunpowder Mixins:
 //
-//    Properties.Java | Add Boolean Property "Supercharged"
+//    Properties.Java | Add Boolean Property "Supercharged" //Or maybe not actually... I don't know much about how mixins are supposed to be used yet
 //
-//    <TNT block behavior> | Make it summon a tnt entity of a delay like, 3 ticks when the redstone input is Supercharged
+//    <TNT block behavior> | Make it summon a tnt entity of a delay 3 ticks when the redstone input is Supercharged
 //
-//            <Gunpowder> | Make it a blockItem, have it place Place GunpowderWireBlock (alt RedstoneWireBlock)
+//    <Gunpowder> | have it place GunpowderWireBlock as an AliasedBlockItem
+//      Minecraft:Items.java line 902: public static final Item GUNPOWDER = Items.register("gunpowder", new Item(new Item.Settings()));
+//      replace with:  public static final Item GUNPOWDER = Items.register("gunpowder", (Item)new AliasedBlockItem(???, new Item.Settings()))
 //
+//    Someone's got to have done something like this before... right?
 //
 //    Maybe Needed?:
 //    Have redstone signals default to Supercharged = false.
