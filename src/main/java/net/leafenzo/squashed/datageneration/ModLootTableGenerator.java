5b57d536e5256d8d0052595d42582abb0422567b
Leafenzo <53666236+Leafenzo@users.noreply.github.com>
2023-03-20 18:16:57 -0600

    start complicated lootTables

--- src/main/java/net/leafenzo/squashed/datageneration/ModLootTableGenerator.java
+++ src/main/java/net/leafenzo/squashed/datageneration/ModLootTableGenerator.java
@@ -3,6 +3,10 @@ package net.leafenzo.squashed.datageneration;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.leafenzo.squashed.block.ModBlocks;
+import net.minecraft.data.server.loottable.BlockLootTableGenerator;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootTableEntry;
 
 public class ModLootTableGenerator extends FabricBlockLootTableProvider {
     public ModLootTableGenerator(FabricDataOutput dataOutput) {
@@ -11,12 +15,22 @@ public class ModLootTableGenerator extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
-        addDrop(ModBlocks.CYAN_DYE_BLOCK);
+        //addDrop(ModBlocks.CYAN_DYE_BLOCK);
         addDrop(ModBlocks.FIRE_CHARGE_BLOCK);
-        //addDrop(ModBlocks.CYAN_DYE_BLOCK, dropsWithSilkTouchOrShears(ModBlocks.CYAN_DYE_BLOCK, null)); //TODO TESTME
-    }
+        //addDrop(ModBlocks.CYAN_DYE_BLOCK, dropsWithSilkTouchOrShears(ModBlocks.CYAN_DYE_BLOCK, null));
 
-    //public LootTableGenerator(FabricDataGenerator dataGen) {
-    //    super(dataGen, LootContextTypes.BLOCK);
-    //}
+        addDrop(ModBlocks.CYAN_DYE_BLOCK, x -> {
+            return dropsWithSilkTouchOrShears(x, ItemEntry.builder(ModBlocks.CYAN_DYE_BLOCK));
+                });
+
+        //BlockLootTableGenerator.dropsWithSilkTouchOrShears(ModBlocks.CYAN_DYE_BLOCK, addDrop(ModBlocks.CYAN_DYE_BLOCK));
+
+        //addDropWithSilkTouch(ModBlocks.FIRE_CHARGE_BLOCK);
+        //leavesDrops()
+
+
+        //TODO
+        // Leaves-like only drop with silk touch
+        // Bookshelf-like drop ingredients
+    }
 }
