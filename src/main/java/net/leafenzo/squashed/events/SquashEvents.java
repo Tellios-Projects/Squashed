ba5ac4cebd2fc48049688ba3fb200cd530413463
Leafenzo <leafenzo@gmail.com>
2023-03-28 10:46:00 -0600

    Start DenseCobweb Fog

---
+++ src/main/java/net/leafenzo/squashed/events/SquashEvents.java
@@ -0,0 +1,10 @@
+package net.leafenzo.squashed.events;
+
+
+/**
+ *  WIP,
+ *  Will be used for detected when a clump of items should be compressed by events in the game world (IE, an anvil falling atop them)
+ */
+public class SquashEvents {
+
+}
