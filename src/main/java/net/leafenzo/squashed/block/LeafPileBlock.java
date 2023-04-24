0fd89d5d32cef4d7816e04d04146fd042ac121b4
Leafenzo <leafenzo@gmail.com>
2023-04-24 14:59:13 -0600

    start leafpileblock behavior

--- src/main/java/net/leafenzo/squashed/block/LeafPileBlock.java
+++ src/main/java/net/leafenzo/squashed/block/LeafPileBlock.java
@@ -1,27 +1,111 @@
 package net.leafenzo.squashed.block;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import net.leafenzo.squashed.util.ModProperties;
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.pathing.NavigationType;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class LeafPileBlock extends Block  {
+public class LeafPileBlock extends LeavesBlock  {
+
+    public static final BooleanProperty SOLID = BooleanProperty.create("solid");
+
     protected static final VoxelShape COLLISION_SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0);
+    private static final float field_31216 = 0.083333336f;
+    private static final float HORIZONTAL_MOVEMENT_MULTIPLIER = 0.9f;
+    private static final float VERTICAL_MOVEMENT_MULTIPLIER = 1.5f;
+    private static final float field_31219 = 2.5f;
+    private static final VoxelShape FALLING_SHAPE = VoxelShapes.cuboid(0.0, 0.0, 0.0, 1.0, 0.9f, 1.0);
+    private static final double field_36189 = 4.0;
+    private static final double SMALL_FALL_SOUND_MAX_DISTANCE = 7.0;
+
     private static final int SCHEDULED_TICK_DELAY = 20;
 
     public LeafPileBlock(AbstractBlock.Settings settings) {
         super(settings);
+        this.setDefaultState((BlockState)this.getDefaultState().with(SOLID, false));
+    }
+
+    //Overriding is NOT deprecated, only calling directly.
+    @Override
+    public boolean isSideInvisible(BlockState state, BlockState stateFrom, Direction direction) {
+        if (stateFrom.isOf(this)) {
+            return true;
+        }
+        return super.isSideInvisible(state, stateFrom, direction);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockState blockBeneath = ctx.getWorld().getBlockState((ctx.getBlockPos().add(0,-1,0))); //TODO TESTME
+        return (BlockState)this.getDefaultState().with(SOLID, blockBeneath.isOf(this) || blockBeneath.isAir() );
+    }
+
+    @Override
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
+        if (world.isClient) {
+            return;
+        }
+        boolean solid = state.get(SOLID);
+        BlockState blockBeneath = world.getBlockState((pos.add(0,-1,0)));
+        if (solid == blockBeneath.isOf(this) || solid == blockBeneath.isAir()) {
+            if (solid) {
+                world.scheduleBlockTick(pos, this, 4);
+            } else {
+                world.setBlockState(pos, (BlockState)state.cycle(SOLID), Block.NOTIFY_LISTENERS);
+            }
+        }
+    }
+
+
+    @Override
+    public VoxelShape getCullingShape(BlockState state, BlockView world, BlockPos pos) {
+        return VoxelShapes.empty();
+    }
+
+    @Override
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
+        if ((double)fallDistance < 4.0 || !(entity instanceof LivingEntity)) {
+            return;
+        }
+        LivingEntity livingEntity = (LivingEntity)entity;
+        LivingEntity.FallSounds fallSounds = livingEntity.getFallSounds();
+        SoundEvent soundEvent = (double)fallDistance < 7.0 ? fallSounds.small() : fallSounds.big();
+        entity.playSound(soundEvent, 1.0f, 1.0f);
     }
 
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return COLLISION_SHAPE;
+        if(state.get(SOLID)) {
+            EntityShapeContext entityShapeContext;
+            Entity entity;
+            if (context instanceof EntityShapeContext && (entity = (entityShapeContext = (EntityShapeContext) context).getEntity()) != null) {
+                if (entity.fallDistance > 2.5f) {
+                    return FALLING_SHAPE;
+                }
+            /*
+            boolean bl = entity instanceof FallingBlockEntity;
+            if (bl || PowderSnowBlock.canWalkOnPowderSnow(entity) && context.isAbove(VoxelShapes.fullCube(), pos, false) && !context.isDescending()) {
+                return super.getCollisionShape(state, world, pos, context);
+            }
+            */
+            }
+            return VoxelShapes.empty();
+        }
+        else { return COLLISION_SHAPE; }
     }
+
 
     @Override
     public VoxelShape getSidesShape(BlockState state, BlockView world, BlockPos pos) {
@@ -35,7 +119,8 @@ public class LeafPileBlock extends Block  {
 
     @Override
     public boolean canPathfindThrough(BlockState state, BlockView world, BlockPos pos, NavigationType type) {
-        return true;
+        if(state.get(SOLID)) {  return true; }
+        return false;
     }
 }
 
---
+++ src/main/java/net/leafenzo/squashed/util/ModProperties.java
@@ -0,0 +1,7 @@
+package net.leafenzo.squashed.util;
+
+import net.minecraft.state.property.BooleanProperty;
+
+public class ModProperties {
+    public static final BooleanProperty SOLID = BooleanProperty.of("solid");
+}
