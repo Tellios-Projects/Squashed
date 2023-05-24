bb78bef4eb2e072cb8285e9a6fd223629649bbd4
Leafenzo <leafenzo@gmail.com>
2023-05-24 03:04:16 -0600

    fix moditemgroup identifiers

--- src/main/java/net/leafenzo/squashed/item/ModItemGroups.java
+++ src/main/java/net/leafenzo/squashed/item/ModItemGroups.java
@@ -11,7 +11,7 @@ import net.minecraft.util.Identifier;
 public class ModItemGroups {
 
     /** Might place each item in a vanilla category? Not sure if that would make porting back harder*/
-    public static final ItemGroup SQUASHED = FabricItemGroup.builder(new Identifier(Super.MOD_ID))
+    public static final ItemGroup SQUASHED = FabricItemGroup.builder(new Identifier(Super.MOD_ID, "squashed"))
             .displayName(Text.literal("Squashed"))
             .icon(() -> new ItemStack(ModBlocks.COMPRESSED_STONE))
             .build();
