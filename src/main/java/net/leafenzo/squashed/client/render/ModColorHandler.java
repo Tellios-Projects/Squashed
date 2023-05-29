7575c161560afc12bbf1b1725912bbc13cc90a8c
Leafenzo <leafenzo@gmail.com>
2023-05-29 16:12:51 -0600

    finish grass and fern blocks
    and ModColorHandler

--- src/main/java/net/leafenzo/squashed/client/render/ModColorHandler.java
+++ src/main/java/net/leafenzo/squashed/client/render/ModColorHandler.java
@@ -4,22 +4,21 @@ package net.leafenzo.squashed.client.render;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.leafenzo.squashed.block.ModBlocks;
 import net.minecraft.client.color.world.BiomeColors;
-import net.minecraft.client.color.world.FoliageColors;
+import net.minecraft.client.color.world.GrassColors;
 import net.minecraft.item.BlockItem;
 
 public class ModColorHandler {
     public static void RegisterBlockColorProviders() {
-        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> pos != null && world != null ? BiomeColors.getFoliageColor(world, pos) : FoliageColors.getDefaultColor(),
+        ColorProviderRegistry.BLOCK.register((state, world, pos, tintIndex) -> pos != null && world != null ? BiomeColors.getGrassColor(world, pos) : GrassColors.getColor(0.5, 1.0), //0.5 and 1.0 are the defaults for grass color (Source: BlockColors line 49)
                 ModBlocks.GRASS_CLIPPINGS_BLOCK,
                 ModBlocks.FERN_BLOCK
-                );
+        );
 
         ColorProviderRegistry.ITEM.register((stack, tintIndex) -> ColorProviderRegistry.BLOCK.get(((BlockItem) stack.getItem()).getBlock()).getColor(((BlockItem) stack.getItem()).getBlock().getDefaultState(), null, null, tintIndex),
                 ModBlocks.GRASS_CLIPPINGS_BLOCK,
                 ModBlocks.FERN_BLOCK
         );
     }
-
 
 
 
