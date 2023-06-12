e1963931d567b0fb2739f0ca9d88c65faba8df86
Leafenzo <leafenzo@gmail.com>
2023-06-12 00:37:59 -0600

    start dense cobweb hud overlay

--- src/main/java/net/leafenzo/squashed/ModClientInit.java
+++ src/main/java/net/leafenzo/squashed/ModClientInit.java
@@ -1,13 +1,16 @@
 package net.leafenzo.squashed;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.leafenzo.squashed.client.render.ModRenderLayers;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.leafenzo.squashed.client.DenseCobwebHudOverlay;
 import net.leafenzo.squashed.client.render.ModColorHandler;
+import net.leafenzo.squashed.client.render.ModRenderLayers;
 
 public class ModClientInit implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModRenderLayers.registerBlockCutouts();
         ModColorHandler.registerBlockColorProviders();
+        HudRenderCallback.EVENT.register(new DenseCobwebHudOverlay());
     }
 }
