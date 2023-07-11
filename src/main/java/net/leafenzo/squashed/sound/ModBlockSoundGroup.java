23eef308620bf2c1b3084d47b8e0ff46250f48e7
Leafenzo <leafenzo@gmail.com>
2023-07-11 01:52:26 -0600

    blaze_rod_block custom SFX

--- src/main/java/net/leafenzo/squashed/sound/ModBlockSoundGroup.java
+++ src/main/java/net/leafenzo/squashed/sound/ModBlockSoundGroup.java
@@ -6,7 +6,11 @@ import net.minecraft.sound.SoundEvents;
 public class ModBlockSoundGroup {
 
     public static final BlockSoundGroup TROPICAL_FISH_BLOCK = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MUD_BREAK, SoundEvents.BLOCK_WOOD_STEP, SoundEvents.BLOCK_MUD_PLACE, SoundEvents.BLOCK_MUD_HIT, SoundEvents.BLOCK_FUNGUS_FALL);
+    public static final BlockSoundGroup BLAZE_ROD_BLOCK  = new BlockSoundGroup(1.0f, 1.0f, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_BREAK, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_STEP, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_PLACE, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_HIT, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_FALL);
+//    public static final BlockSoundGroup BLAZE_ROD_BLOCK  = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_STONE_BREAK, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_STEP, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_PLACE, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_HIT, SoundEvents.BLOCK_STONE_FALL);
 
-    //public static final BlockSoundGroup MUD = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MUD_BREAK, SoundEvents.BLOCK_MUD_STEP, SoundEvents.BLOCK_MUD_PLACE, SoundEvents.BLOCK_MUD_HIT, SoundEvents.BLOCK_MUD_FALL);
-   // public static final BlockSoundGroup FUNGUS = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_FUNGUS_BREAK, SoundEvents.BLOCK_FUNGUS_STEP, SoundEvents.BLOCK_FUNGUS_PLACE, SoundEvents.BLOCK_FUNGUS_HIT, SoundEvents.BLOCK_FUNGUS_FALL);
+//    public static final BlockSoundGroup TROPICAL_FISH_BLOCK = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MUD_BREAK, SoundEvents.BLOCK_WOOD_STEP, SoundEvents.BLOCK_MUD_PLACE, SoundEvents.BLOCK_MUD_HIT, SoundEvents.BLOCK_FUNGUS_FALL); MUD = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MUD_BREAK, SoundEvents.BLOCK_MUD_STEP, SoundEvents.BLOCK_MUD_PLACE, SoundEvents.BLOCK_MUD_HIT, SoundEvents.BLOCK_MUD_FALL);
+//    public static final BlockSoundGroup FUNGUS = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_FUNGUS_BREAK, SoundEvents.BLOCK_FUNGUS_STEP, SoundEvents.BLOCK_FUNGUS_PLACE, SoundEvents.BLOCK_FUNGUS_HIT, SoundEvents.BLOCK_FUNGUS_FALL);
+//    public static final BlockSoundGroup SHROOMLIGHT = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_SHROOMLIGHT_BREAK, SoundEvents.BLOCK_SHROOMLIGHT_STEP, SoundEvents.BLOCK_SHROOMLIGHT_PLACE, SoundEvents.BLOCK_SHROOMLIGHT_HIT, SoundEvents.BLOCK_SHROOMLIGHT_FALL);
+
 }
