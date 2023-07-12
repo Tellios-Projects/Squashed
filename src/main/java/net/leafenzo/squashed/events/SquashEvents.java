d0444a9f17f3e9f64f12a68250d8183f3f347de7
Leafenzo <leafenzo@gmail.com>
2023-07-12 01:44:37 -0600

    some notes on Squash Events

--- src/main/java/net/leafenzo/squashed/events/SquashEvents.java
+++ src/main/java/net/leafenzo/squashed/events/SquashEvents.java
@@ -6,5 +6,19 @@ package net.leafenzo.squashed.events;
  *  Will be used for detected when a clump of items should be compressed by events in the game world (IE, an anvil falling atop them)
  */
 public class SquashEvents {
+    private void Squash(int power) {
 
+    }
+    // use .json recipes btw
+
+    /*
+    Triggers:
+     - Anvil Falling on block (power 3)
+     - Piston Crushing Item Against Wall (power 3)
+     - Stepped on by Iron Golem (power 3)
+     - Stepped on by Ravager (power 3)
+     - Jumped on by Large Slime or Large Magma Cube (power 3)
+     - Jumped on by player wearing Squashing Enchanted boots (power 3-5, depending on enchantment level)
+     - Orbiting Quintessence (power 4-6, depending on Quintessense size)
+     */
 }
