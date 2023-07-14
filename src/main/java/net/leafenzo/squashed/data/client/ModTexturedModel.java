bd1f0306d66145dac84a7ffbb2c8c315bf5d154f
Leafenzo <leafenzo@gmail.com>
2023-07-13 18:56:04 -0600

    fix logPile's (again)

--- src/main/java/net/leafenzo/squashed/data/client/ModTexturedModel.java
+++ src/main/java/net/leafenzo/squashed/data/client/ModTexturedModel.java
@@ -1,23 +1,14 @@
-package net.leafenzo.squashed.util;
+package net.leafenzo.squashed.data.client;
 
 import net.minecraft.block.Block;
-import net.minecraft.data.client.*;
-import net.minecraft.util.Identifier;
-
-import java.util.Optional;
+import net.minecraft.data.client.Models;
+import net.minecraft.data.client.TextureKey;
+import net.minecraft.data.client.TextureMap;
+import net.minecraft.data.client.TexturedModel;
 
 public class ModTexturedModel {
-    public static final TexturedModel.Factory LOG_PILE = TexturedModel.makeFactory(ModTexturedModel::logPile, Models.CUBE_DIRECTIONAL);
+    public static final TexturedModel.Factory LOG_PILE = TexturedModel.makeFactory(ModTexturedModel::logPile, ModModels.CUBE_DIRECTIONAL_DOWN_ROTATED);
     public static final TexturedModel.Factory CUBE_COLUME_SIDE_DOUBLE_FRONT = TexturedModel.makeFactory(ModTexturedModel::cubeColumnSideDoubleFront, Models.CUBE);
-
-    public static final Model CUBE_DIRECTIONAL_DOWN_ROTATED = ModTexturedModel.block("minecraft:block/cube_directional", TextureKey.PARTICLE, TextureKey.NORTH, TextureKey.SOUTH, TextureKey.EAST, TextureKey.WEST, TextureKey.UP, TextureKey.DOWN);
-
-
-
-    private static Model block(String path, TextureKey ... requiredTextureKeys) {
-        return new Model(Optional.of(new Identifier(path)), Optional.empty(), requiredTextureKeys);
-    }
-
 
     public static TextureMap logPile(Block block) {
         return  new TextureMap()
