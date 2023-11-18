edc23ff090a4dd270e11bc11d918db227d47207a
Leafenzo <leafenzo@gmail.com>
2023-11-18 16:32:39 -0700

    Add Experience Block BlockSoundGroup

--- src/main/java/net/leafenzo/squashed/sound/ModBlockSoundGroup.java
+++ src/main/java/net/leafenzo/squashed/sound/ModBlockSoundGroup.java
@@ -6,7 +6,8 @@ import net.minecraft.sound.SoundEvents;
 public class ModBlockSoundGroup {
 
     public static final BlockSoundGroup TROPICAL_FISH_BLOCK = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MUD_BREAK, SoundEvents.BLOCK_WOOD_STEP, SoundEvents.BLOCK_MUD_PLACE, SoundEvents.BLOCK_MUD_HIT, SoundEvents.BLOCK_FUNGUS_FALL);
-    public static final BlockSoundGroup BLAZE_ROD_BLOCK  = new BlockSoundGroup(1.0f, 1.0f, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_BREAK, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_STEP, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_PLACE, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_HIT, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_FALL);
+    public static final BlockSoundGroup BLAZE_ROD_BLOCK = new BlockSoundGroup(1.0f, 1.0f, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_BREAK, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_STEP, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_PLACE, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_HIT, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_FALL);
+    public static final BlockSoundGroup EXPERIENCE_BLOCK = new BlockSoundGroup(1.0f, 2.0f, SoundEvents.BLOCK_AMETHYST_CLUSTER_BREAK, SoundEvents.BLOCK_AMETHYST_CLUSTER_STEP, SoundEvents.BLOCK_AMETHYST_CLUSTER_PLACE, SoundEvents.BLOCK_AMETHYST_CLUSTER_HIT, SoundEvents.BLOCK_AMETHYST_CLUSTER_FALL);
 //    public static final BlockSoundGroup BLAZE_ROD_BLOCK  = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_STONE_BREAK, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_STEP, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_PLACE, ModSoundEvents.BLOCK_BLAZE_ROD_BLOCK_HIT, SoundEvents.BLOCK_STONE_FALL);
 
 //    public static final BlockSoundGroup TROPICAL_FISH_BLOCK = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MUD_BREAK, SoundEvents.BLOCK_WOOD_STEP, SoundEvents.BLOCK_MUD_PLACE, SoundEvents.BLOCK_MUD_HIT, SoundEvents.BLOCK_FUNGUS_FALL); MUD = new BlockSoundGroup(1.0f, 1.0f, SoundEvents.BLOCK_MUD_BREAK, SoundEvents.BLOCK_MUD_STEP, SoundEvents.BLOCK_MUD_PLACE, SoundEvents.BLOCK_MUD_HIT, SoundEvents.BLOCK_MUD_FALL);
